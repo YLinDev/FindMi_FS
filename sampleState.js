e58783daaebd71502ad2a91dc3b9f4ed4064aac9
Yong <yong.lin13@gmail>
2023-03-27 21:16:20 -0400

    frontend setup

---
+++ sampleState.js
@@ -0,0 +1,34 @@
+{
+	entities: {
+		users: {
+			1: {
+				id: 1, 
+				email: "user1@findmi.com"
+			},
+
+			2: {
+				id: 2,
+				email: "user2@findmi.com"
+			}
+		},
+
+		listings: {
+			1: {
+				id: 1, 
+				price: 100000,
+				bedrooms: 2,
+				bathrooms: 2,
+				sqft: 800,
+				address: '123 Main Street New York, NY 10001',
+				listing_type: 'sale',
+				year_built: '2000',
+				description: "Very nice home, don't miss this great home"
+				condo: false,
+				air_cond: 'window',
+				parking: '1 garage',
+				monthly_hoa_fee: 0,
+				price_per_sqft: 
+			}
+		}
+	}
+}
