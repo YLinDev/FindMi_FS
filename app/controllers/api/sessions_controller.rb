c4c4b68b5b4cc9543dc5f7cd34de66f0600fe55e
Yong <yong.lin13@gmail>
2023-03-31 14:32:47 -0400

    finished user auth backend

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Api::SessionsController < ApplicationController
+  def show
+    if current_user
+      @user = current_user 
+      render 'api/users/show'
+    else
+      render json: { user: nil }
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:email],
+      params[:password]
+    )
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else 
+      render json: { errors: ["Incorrect email or password. Please try again or click 'Demo User'."]}, status: 400
+    end
+  end
+
+  def destroy
+    if current_user 
+      logout!
+      render json: { message: 'Logged-Out'}
+    end
+  end
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::API
       with: :invalid_authenticity_token
     before_action :snake_case_params, :attach_authenticity_token
 
-
-
     def current_user
         return nil unless session[:session_token] 
         @current_user = User.find_by(session_token: session[:session_token])# user whose `session_token` == token in `session` cookie
@@ -27,6 +25,20 @@ class ApplicationController < ActionController::API
     def require_logged_in
         unless current_user
             render json: { message: 'Unauthorized' }, status: :unauthorized 
+        end
+    end
+
+    def test
+        if params.has_key?(:login)
+          login!(User.first)
+        elsif params.has_key?(:logout)
+          logout!
+        end
+      
+        if current_user
+          render json: { user: current_user.slice('id', 'email', 'session_token') }
+        else
+          render json: ['No current user']
         end
     end
 
