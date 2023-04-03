7fe82d459e0d3c9aa2bf5e6d9c2f4da4620453d7
Yong <yong.lin13@gmail>
2023-04-03 14:51:27 -0400

    render update

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,9 @@ Bundler.require(*Rails.groups)
 
 module FindMi
   class Application < Rails::Application
-    config.railties_order = [:all, :main_app]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.railties_order = [:all, :main_app]
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -35,14 +35,11 @@ module FindMi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    module Backend
-      class Application < Rails::Application
-        config.middleware.use ActionDispatch::Cookies
-        config.middleware.use ActionDispatch::Session::CookieStore,
-          key: '_findMi_session',
-          same_site: :lax, 
-          secure: Rails.env.production?
-      end
-    end
+    config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_findMi_session',
+      same_site: :lax, 
+      secure: Rails.env.production?
   end
 end
