6f7ca951c36c0be2bc167c1566c44358a8a219c3
YLinDev <adikit1@icloud.com>
2023-05-11 13:17:03 -0400

    fix deadlinks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import ShowListing from "./components/ShowListing";
 import UserListings from "./components/UserListings";
 import SavedHomes from "./components/SavedHomes";
 import Footer from "./components/Footer";
+import Future from "./components/Future";
 
 
 function App() {
@@ -21,6 +22,9 @@ function App() {
             <FirstCollection />
             <IntroBar />
             <Footer />
+          </Route>
+          <Route exact path="/future">
+            <Future />
           </Route>
           <Route exact path="/home">
             <Home />
---
+++ frontend/src/components/Future/index.js
@@ -0,0 +1,13 @@
+function Future() {
+
+    return (
+        <>
+            <div style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
+                <h2 style={{fontSize: "30px"}}>To be implemented, stay tuned</h2>
+                <img src={require("./assets/rabbit.gif")} style={{width: "50%"}}/>
+            </div>
+        </>
+    )
+}
+
+export default Future
