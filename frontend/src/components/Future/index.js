37f17e845cbf1ca65340dfc4d1f15338dc077097
YLinDev <adikit1@icloud.com>
2023-05-30 14:47:47 -0400

    edit

--- frontend/src/components/Future/index.js
+++ frontend/src/components/Future/index.js
@@ -3,7 +3,7 @@ function Future() {
     return (
         <>
             <div style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
-                <h2 style={{fontSize: "30px"}}>To be implemented, stay tuned</h2>
+                <h2 style={{fontSize: "30px"}}>To be implemented, please stay tuned</h2>
                 <img src={require("./assets/rabbit.gif")} style={{width: "50%"}}/>
             </div>
         </>
